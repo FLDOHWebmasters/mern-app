7f0490ee709fd7bba781a87a553505459d4e6bf0
FLDOHWebmasters <51002860+FLDOHWebmasters@users.noreply.github.com>
2021-02-26 15:33:33 -0500

    Added config file

---
+++ src/Config.js
@@ -0,0 +1,9 @@
+export const config = {
+    appId: '8b394bd2-4206-460f-886c-cb1e5d50bc00',
+    redirectUri: 'https://black-stone-02eb0230f.azurestaticapps.net',
+    scopes: [
+      'user.read',
+      'mailboxsettings.read',
+      'calendars.readwrite'
+    ]
+  };
