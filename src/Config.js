65135cefff626b64e3fa34e4cffa74888acae281
FLDOHWebmasters <51002860+FLDOHWebmasters@users.noreply.github.com>
2021-06-02 22:58:27 -0400

    Changed Redirect URL for Signin

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,6 @@
 export const config = {
     appId: '8b394bd2-4206-460f-886c-cb1e5d50bc00',
-    redirectUri: 'http://localhost:3000',
+    redirectUri: '/',
     scopes: [
       'user.read',
       'mailboxsettings.read',
