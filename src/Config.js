529ad8d70c8a2dfa3dfa4b8dc15f1a52cc071a1b
FLDOHWebmasters <51002860+FLDOHWebmasters@users.noreply.github.com>
2021-06-03 08:19:44 -0400

    Update Config.js

--- src/Config.js
+++ src/Config.js
@@ -1,6 +1,6 @@
 export const config = {
-    appId: '8b394bd2-4206-460f-886c-cb1e5d50bc00',
-    redirectUri: '/',
+    appId: 'c5434299-b6b6-4f72-a55b-f4b16b871262',
+    redirectUri: 'https://lively-hill-0d8298410.azurestaticapps.net/.auth/login/aad/callback',
     scopes: [
       'user.read',
       'mailboxsettings.read',
